175faa02384270efc7403d11cb3e03cbd9d8948c
hnifmaghfur <hnifmaghfur@gmail.com>
2020-09-26 09:10:21 +0700

    add week4, and costumized week2

--- Week 2/materi/function.js
+++ Week 2/materi/function.js
@@ -1,22 +1,22 @@
 const nilai = [
-    {
-        nama: 'Hanif Maghfur',
-        usia: 25
-    },
-    {
-        nama: 'Winda Aprilia',
-        usia: 25
-    },
-    {
-        nama: 'Faris Abdullah',
-        usia: 13
-    },
-]
+  {
+    nama: "Hanif Maghfur",
+    usia: 25,
+  },
+  {
+    nama: "Arungi Cahaya",
+    usia: 7,
+  },
+  {
+    nama: "Saidina Hamzah",
+    usia: 13,
+  },
+];
 
 const panggil = (params) => {
-    //params[1] berarti index ke 1 (ingat index dimulai dari 0)
-    const winda = params[1]
-    console.log(winda.nama);
-}
+  //params[1] berarti index ke 1 (ingat index dimulai dari 0)
+  const tugas = params[0];
+  console.log(tugas.nama);
+};
 
 panggil(nilai);
